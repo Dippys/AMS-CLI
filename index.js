4af9a281a482643033b880d07dd5b4d66baaf7f9
Dippys <ahmed@barrawi.xyz>
2022-12-10 16:44:06 +0400

    Welp

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 const updater = require('./helpers/updater');
 const fs = require('fs');
 
+// how is the __dirname variable defined?
+// https://stackoverflow.com/questions/8131344/what-is-the-difference-between-dirname-and-in-node-js
+
 if (!fs.existsSync(__dirname + '/cache')) {
     fs.mkdirSync(__dirname + '/cache');
 }
 
-if (!fs.existsSync('/bin') {
+if (!fs.existsSync('/bin')) {
     fs.mkdirSync('./bin');
 }
 
