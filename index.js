5fb30a55daad685d719843988f4582a8b5612e67
Dippys <ahmed@barrawi.xyz>
2022-12-10 16:41:09 +0400

    I'm just dumb

--- index.js
+++ index.js
@@ -3,16 +3,13 @@
 const updater = require('./helpers/updater');
 const fs = require('fs');
 
-// if it doesn't exist, create the cache folder and bin folder
 if (!fs.existsSync(__dirname + '/cache')) {
-    fs.mkdirSync(__dirname + + '/cache');
+    fs.mkdirSync(__dirname + '/cache');
 }
 
-// check if this is being run as a global package or not
-if (!fs.existsSync(__dirname + '/bin')) {
+if (!fs.existsSync('/bin') {
     fs.mkdirSync('./bin');
 }
-
 
 
 updater.execute();
